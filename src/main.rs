e94284ddf3bccccfbee3fccc2397df787cf989a2
RizalAchp <rizal.ahmadp@gmail.com>
2022-11-10 12:56:00 +0700

    Update: Add more Functionality

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,109 @@
-fn main() {
-    let mut args = std::env::args();
-    let mode = args
-        .nth(1)
-        .expect("need more arguments, [mode : 1, 2]")
-        .parse()
-        .unwrap_or(0);
-    let file_one = args.next().expect("provide file in arguments 2");
-    let file_two = args.next().expect("provide file in arguments 3");
-    let file_output = args.next().unwrap_or("output.xlsx".to_owned());
-    eprintln!("running mode: {}", mode);
-    if mode == 1 {
-        let result = get_error(file_one, file_two, None, None);
-        convert_csv_to_excel(result, file_output, "Sheet1".to_owned());
-    } else if mode == 2 {
-        let result = change_row(file_one, 3, 2);
-        convert_csv_to_excel(result, file_output, "Sheet1".to_owned());
-    } else {
-        let result = change_from_one_to_another(file_one, file_two, 4, 5);
-        convert_csv_to_excel(result, file_output, "Sheet1".to_owned());
-    }
+use std::path::{Path, PathBuf};
+
+use anyhow::{anyhow, Result};
+use clap::*;
+
+/// Simple program manage and generate excel file
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct CliArgs {
+    /// file excel a to process
+    file_a: PathBuf,
+    /// file excel b to process
+    #[clap(default_value = "file_target.xlsx")]
+    file_b: PathBuf,
+    /// file excel b to process
+    #[clap(short, long, default_value = "output.xlsx")]
+    output: PathBuf,
+
+    #[command(subcommand)]
+    action: Action,
 }
 
-fn change_from_one_to_another(
-    one: String,      // file two
-    two: String,      // file two
-    ito_one_a: usize, // index to compare in file one
-    ito_one_b: usize, // index to compare in file one
-) -> Vec<Vec<String>> {
-    let mut exc_one = open_excel(&one, None);
-    let exc_two = open_excel(&two, None);
+#[derive(clap::Subcommand, Debug)]
+enum Action {
+    /// Move Row from match on comparing row from file two to one
+    Move {
+        /// indicies to move from
+        #[clap(short, long, required = true)]
+        a: Vec<usize>,
+        /// indicies to move into
+        #[clap(short, long, required = true)]
+        b: Vec<usize>,
+        /// indicies to compare
+        #[clap(short, long, required = true)]
+        idx: Vec<usize>,
+    },
+    /// Modifie Row from maatch on compareing row from single file
+    Mod,
+    /// check if row in file a is exist in row file b
+    Check {
+        /// indicies to compare
+        #[clap(short, long)]
+        idx: Vec<usize>,
+    },
+}
 
-    exc_one.iter_mut().for_each(|row_one| {
-        for row_two in exc_two.iter() {
-            if row_two.contains(&row_one[ito_one_a]) && row_two.contains(&row_one[ito_one_b]) {
-                row_one[3] = row_two[1].clone();
-                row_one[6] = row_two[4].clone();
-                row_one[7] = row_two[3].clone();
-            }
+fn main() -> Result<()> {
+    let args = CliArgs::parse();
+    match args.action {
+        Action::Move { a, b, idx } => {
+            let result = move_row_and_compare(&args.file_a, &args.file_b, a, b, |(a, b)| {
+                idx.iter().all(|i| b.contains(&a[*i]))
+            })?;
+            convert_csv_to_excel(result, args.output, "Sheet1".to_owned());
         }
-    });
+        Action::Mod => {
+            let result = change_row(args.file_a, 3, 2)?;
+            convert_csv_to_excel(result, args.output, "Sheet1".to_owned());
+        }
+        Action::Check { idx } => {
+            let result = get_error(args.file_a, args.file_b, |(a, b)| {
+                idx.iter().all(|i| b.contains(&a[*i]))
+            })?;
+            convert_csv_to_excel(result, args.output, "Sheet1".to_owned());
+        }
+    }
+    Ok(())
+}
 
-    exc_one
+fn move_row_and_compare<IT, CB, S>(
+    file_a: S,
+    file_b: S,
+    it_a: IT,
+    it_b: IT,
+    compare: CB,
+) -> Result<Vec<Vec<String>>>
+where
+    IT: IntoIterator<Item = usize> + Clone,
+    S: AsRef<Path>,
+    CB: Fn((&Vec<String>, &Vec<String>)) -> bool,
+{
+    let mut exc_a = open_excel(file_a.as_ref().to_str().unwrap(), None)?;
+    let exc_b = open_excel(file_b.as_ref().to_str().unwrap(), None)?;
+
+    for row_a in exc_a.iter_mut() {
+        for row_b in exc_b.iter() {
+            if !compare((&row_a, row_b)) {
+                continue;
+            }
+            it_a.clone()
+                .into_iter()
+                .zip(it_b.clone())
+                .for_each(|(a, b)| {
+                    row_a[a] = row_b[b].clone();
+                });
+        }
+    }
+    Ok(exc_a)
 }
 
 #[allow(unused)]
-fn change_row(file: String, idx_a: usize, idx_b: usize) -> Vec<Vec<String>> {
-    let mut file_excel = open_excel(&file, None);
+fn change_row<P>(file: P, idx_a: usize, idx_b: usize) -> Result<Vec<Vec<String>>>
+where
+    P: AsRef<Path>,
+{
+    let mut file_excel = open_excel(file.as_ref().to_str().unwrap(), None)?;
     let changed: Vec<Vec<String>> = file_excel
         .iter_mut()
         .map(|item| {
@@ -61,36 +119,30 @@ fn change_row(file: String, idx_a: usize, idx_b: usize) -> Vec<Vec<String>> {
         })
         .collect();
 
-    return changed;
+    Ok(changed)
 }
 
 #[allow(unused)]
-fn get_error(
-    file_1: String,
-    file_2: String,
-    idx_one: Option<usize>,
-    idx_two: Option<usize>,
-) -> Vec<Vec<String>> {
-    let idx_one = idx_one.unwrap_or(5);
-    let idx_two = idx_two.unwrap_or(4);
-    let mut excel_one = open_excel(&file_1, None);
-    let excel_two = open_excel(&file_2, None);
+fn get_error<P, CB>(file_1: P, file_2: P, callback: CB) -> Result<Vec<Vec<String>>>
+where
+    P: AsRef<Path>,
+    CB: Fn((&Vec<String>, &Vec<String>)) -> bool,
+{
+    let mut excel_one = open_excel(file_1.as_ref().to_str().unwrap(), None)?;
+    let excel_two = open_excel(file_2.as_ref().to_str().unwrap(), None)?;
 
     let mut to_delete = vec![true; excel_one.len()];
-    for (i, set_a) in excel_one.iter().enumerate().rev() {
-        for set_b in excel_two.iter() {
-            if !set_b.contains(&set_a[idx_one])
-                && !(set_b[idx_two].to_ascii_lowercase() == set_a[idx_one].to_ascii_lowercase())
-            {
-                continue;
+    for (i, row_a) in excel_one.iter().enumerate().rev() {
+        for row_b in excel_two.iter() {
+            if callback((row_a, row_b)) {
+                to_delete[i] = false;
             }
-            to_delete[i] = false;
         }
     }
     let mut iter_delete = to_delete.iter();
     excel_one.retain(|_| *iter_delete.next().unwrap_or_else(|| &true));
     eprintln!("size error: {}", excel_one.len());
-    return excel_one;
+    Ok(excel_one)
 }
 
 #[allow(unused)]
@@ -99,7 +151,7 @@ pub(crate) fn convert_csv_to_excel<P>(
     excel_path: P,
     sheets_name: String,
 ) where
-    P: AsRef<std::path::Path>,
+    P: AsRef<Path>,
 {
     let mut wb = simple_excel_writer::Workbook::create(
         excel_path.as_ref().to_str().unwrap_or("output.xlsx"),
@@ -127,17 +179,17 @@ pub fn deserialize_data_excel(range: &calamine::Range<calamine::DataType>) -> Ve
     // let mut dest = String::new();
     let mut out = Vec::new();
     out.reserve(range.get_size().0);
-    for r in range.rows() {
+    for rows in range.rows() {
         let mut row = Vec::new();
-        row.reserve(r.len());
-        for c in r.iter() {
+        row.reserve(rows.len());
+        for c in rows.iter() {
             match *c {
                 DataType::Empty => row.push("-".to_owned()),
                 DataType::String(ref s) => row.push(s.trim().to_owned().to_uppercase()),
                 DataType::Float(ref f) | DataType::DateTime(ref f) => row.push(f.to_string()),
                 DataType::Int(ref i) => row.push(i.to_string()),
                 DataType::Error(ref e) => row.push(e.to_string().to_uppercase()),
-                DataType::Bool(ref b) => row.push(b.to_string().to_uppercase()),
+                DataType::Bool(ref b) => row.push(b.to_string()),
             };
         }
         out.push(row);
@@ -145,20 +197,19 @@ pub fn deserialize_data_excel(range: &calamine::Range<calamine::DataType>) -> Ve
     out
 }
 
-fn open_excel(path: &str, sheets: Option<&str>) -> Vec<Vec<String>> {
+fn open_excel(path: &str, sheets: Option<&str>) -> Result<Vec<Vec<String>>> {
     use calamine::*;
-    let mut sheet = open_workbook_auto(&path).expect("cant open Workbook");
+    let mut sheet =
+        open_workbook_auto(&path).map_err(|d| anyhow!("Error on opening workbook excel! {}", d))?;
     if let Some(sh) = sheets {
         let data = sheet
             .worksheet_range(sh)
-            .expect("error on opening sheets")
-            .expect("Error on processing data");
-        deserialize_data_excel(&data)
+            .expect("error on opening sheets")?;
+        Ok(deserialize_data_excel(&data))
     } else {
         let data = sheet
             .worksheet_range_at(0)
-            .expect("error on opening sheets")
-            .expect("Error on processing data");
-        deserialize_data_excel(&data)
+            .expect("Error on processing data")?;
+        Ok(deserialize_data_excel(&data))
     }
 }
